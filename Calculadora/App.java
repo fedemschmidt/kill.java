33ef0a4a9e01c4a4ee250ce9c191cf7ee7df89e2
fedemschmidt <38530984+fedemschmidt@users.noreply.github.com>
2019-07-10 10:12:09 -0300

    Add files via upload

--- Calculadora/App.java
+++ Calculadora/App.java
@@ -25,69 +25,74 @@ public final class App {
         c.addRutina(r2);
         c.addRutina(r3);
 
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 20
+        r1.addinstrucion("push"); // ingresando 10
+        r1.addinstrucion("push"); // ingresando 5
+        r1.addinstrucion("push"); // ingresando 5
         r1.addinstrucion("add");
         r1.addinstrucion("add");
-        r1.addinstrucion("sub");
+        r1.addinstrucion("sub"); // devuelve 0
 
-        r2.addinstrucion("push");
+        r2.addinstrucion("push"); 
         r2.addinstrucion("mul");
-        r2.addinstrucion("write");
+        r2.addinstrucion("write"); // coloca un 0 en la memoria
 
-        r3.addinstrucion("push");
-        r3.addinstrucion("read");
-        r3.addinstrucion("sum");
-        r3.addinstrucion("write");*/
+        r3.addinstrucion("push"); // ingresando 5
+        r3.addinstrucion("read"); // usando el nombre del write anterior
+        r3.addinstrucion("sum"); 
+        r3.addinstrucion("write"); // coloca un 5 en la memoria
+        
+        c.uasrRutina(0);
+        c.usarRutina(1);
+        c.ussarRutina(2);*/ 
+
 
         /* Test push
         Calculadora c = new Calculadora();
         Rutinas r1 = new Rutinas();
         c.addRutina(r1);
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 5
         c.usarRutina(0);
-        if(c.check()==5) System.out.println("!=/!="); */
+        if(c.check()==5) System.out.println("works"); */
 
         // Test add
         /*Calculadora c = new Calculadora();
         Rutinas r1 = new Rutinas();
         c.addRutina(r1);
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 5
+        r1.addinstrucion("push"); // ingresando 5
         r1.addinstrucion("add");
         c.usarRutina(0);
-        if(c.check()==5) System.out.println("!=/!=");*/
+        if(c.check()==10) System.out.println("works");*/
 
         // Test sub
         /*Calculadora c = new Calculadora();
         Rutinas r1 = new Rutinas();
         c.addRutina(r1);
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 5
+        r1.addinstrucion("push"); // ingresando 10
         r1.addinstrucion("sub");
         c.usarRutina(0);
-        if(c.check()==5) System.out.println("!=/!=");*/
+        if(c.check()==5) System.out.println("works");*/
 
         // Test mul
         /*Calculadora c = new Calculadora();
         Rutinas r1 = new Rutinas();
         c.addRutina(r1);
-        r1.addinstrucion("push");
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 5
+        r1.addinstrucion("push"); // ingresando 5
         r1.addinstrucion("mul");
         c.usarRutina(0);
-        if(c.check()==25) System.out.println("!=/!=");*/
+        if(c.check()==25) System.out.println("works");*/
 
         //Test write and read
         /*Calculadora c = new Calculadora();
         Rutinas r1 = new Rutinas();
         c.addRutina(r1);
-        r1.addinstrucion("push");
+        r1.addinstrucion("push"); // ingresando 5
         r1.addinstrucion("write");
         r1.addinstrucion("read");
         c.usarRutina(0);
-        System.out.println(c.check());*/
+        if(c.check()==5) System.out.println("works");*/
     }
 }
